2a52db2b9c159b35db28086f8920639de2d3fa70
6fernando9 <fernandopadilla170@gmail.com>
2025-12-19 08:21:58 -0400

    fix:agregando campo de usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,10 @@ class User extends Authenticatable
         'email',
         'password',
         'rol', // barbero or cliente
+        'estado',
+        'apellido',
+        'telefono',
+    
     ];
 
     /**
